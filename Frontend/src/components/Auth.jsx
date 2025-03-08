944046f4e3fb5848a4c5df3997c27b43941e5db0
bhatt04 <meghabhatt0403@gmail.com>
2025-03-09 00:35:50 +0530

    FireBase Setup

---
+++ Frontend/src/components/Auth.jsx
@@ -0,0 +1,35 @@
+import { auth, GoogleProvider } from "../config/firebase";
+import { useState } from "react"; 
+import { createUserWithEmailAndPassword, signInWithPopup} from 'firebase/auth';
+
+export const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const signIn = async () => {
+        try{
+            await createUserWithEmailAndPassword(auth, email, password);
+        }catch(err){
+            console.log(err);
+        }
+    };
+
+    const signInWithGoogle = async () => {
+        try{
+            await signInWithPopup(auth, GoogleProvider);
+        }catch(err){
+            console.log(err);
+        }
+    };
+
+
+    return <div>
+        <input placeholder="email" onChange={(e) => setEmail(e.target.value)}/>
+        <input placeholder="password" type="password" onChange={(e) => setPassword(e.target.value)}/>
+        <button onClick={signIn}>Sign In</button>
+
+        <button onClick={signInWithGoogle}> Sign-in with Google </button>
+    </div>
+};
+
+//26
